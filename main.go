0f47950fa9874a1ccc540decc805f9e03f7a0ba7
Kushagra Gupta <kushagra.kingg@live.com>
2024-01-24 23:34:45 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	fmt.Println(links)
+}
